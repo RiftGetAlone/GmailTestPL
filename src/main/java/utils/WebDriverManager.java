4b7bffd2a902a1dbedb4d4ed35fbed853661e9a9
riftgetalone <fort.kto2@gmail.com>
2019-06-20 11:39:08 +0400

    Final version

--- src/main/java/utils/WebDriverManager.java
+++ src/main/java/utils/WebDriverManager.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 
 public  class WebDriverManager {
     private static final String defaultChromeDriverPath = "src\\main\\resources\\drivers\\chromedriver.exe";
-    private static final String defaultFirefoxDriverPath = "src\\main\\resources\\drivers\\geckodriver.exe";
 
     public static WebDriver initChromeDriver() {
         System.setProperty("webdriver.chrome.driver", defaultChromeDriverPath);
@@ -23,20 +22,5 @@ public  class WebDriverManager {
     public static WebDriver initChromeDriver(String browserPath, String webDriverPath) {
         System.setProperty("webdriver.chrome.driver", webDriverPath);
         return  new ChromeDriver(new ChromeOptions().setBinary(browserPath));
-    }
-
-    public static WebDriver initFirefoxDriver() {
-        System.setProperty("webdriver.gecko.driver", defaultFirefoxDriverPath);
-        return new FirefoxDriver();
-    }
-
-    public static WebDriver initFirefoxDriver(String browserPath) {
-        System.setProperty("webdriver.gecko.driver", defaultFirefoxDriverPath);
-        return  new FirefoxDriver(new FirefoxOptions().setBinary(browserPath));
-    }
-
-    public static WebDriver initFirefoxDriver(String browserPath, String webDriverPath) {
-        System.setProperty("webdriver.gecko.driver", webDriverPath);
-        return  new FirefoxDriver(new FirefoxOptions().setBinary(browserPath));
     }
 }
